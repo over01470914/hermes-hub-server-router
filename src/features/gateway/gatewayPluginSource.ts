18cfabc1369d61acd526c8bd5930d9abde5f6a7b
GarbaGod <over01470914@qq.com>
2026-07-29 04:37:48 +0800

    release(router): prepare v1.0.1

--- src/features/gateway/gatewayPluginSource.ts
+++ src/features/gateway/gatewayPluginSource.ts
@@ -8,8 +8,8 @@ export const gatewayPluginRepositoryUrl =
 
 export const gatewayPluginNpmPackage = Object.freeze({
   name: '@over01470914/hermes-hub-gateway',
-  version: '0.4.17',
-  runtimeManifestSha256: '6070d58491c441cbdfc9a50fb5b998b9cc30b1926e33974bc1091f5ccdf636d2',
+  version: '1.0.1',
+  runtimeManifestSha256: '5a4b23be0eae80448f7c874243cbed7075dc0db21bac775f130e168756200caf',
 })
 
 export const gatewayPluginReleaseArtifact = Object.freeze({
